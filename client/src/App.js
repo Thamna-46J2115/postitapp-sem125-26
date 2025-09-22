c93441343962528e9c67e53c643d32ead653faa0
Thamna-46J2115 <46J2115@utas.edu.om>
2025-09-22 13:35:56 +0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Home /> */}
+      <Login />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/Components/Login.js
@@ -0,0 +1,12 @@
+import loginimage from "../Images/loginImage.jpg";
+import "../App.css";
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <img src={loginimage} className="loginsmall"></img>
+    </div>
+  );
+};
+
+export default Login;
