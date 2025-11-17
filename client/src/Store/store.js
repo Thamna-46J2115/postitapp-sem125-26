36d7f3c1a2f0fe73f1d08e300ba7ffd4c86a0643
Thamna-46J2115 <46J2115@utas.edu.om>
2025-11-17 13:47:46 +0400

    act12

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -1,10 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
-import usersReducer from "../Features/UserSlice"; //import the reducer
+import usersReducer from "../Features/UserSlice";
 import postReducer from "../Features/PostSlice";
-export const store = configureStore({
-  reducer: {
-    users: usersReducer,
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
 
-    posts: postReducer,
-  },
+// Redux Persist config
+const persistConfig = {
+  key: "reduxstore",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  users: usersReducer,
+  posts: postReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistore = persistStore(store);
+
+export { store, persistore };
